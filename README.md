6bd7ec056feb01d46e359bba48038b7e8c205470
Shivam Rana <86354964+shivamranaaa@users.noreply.github.com>
2021-09-01 13:39:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Deployment of salaray-predict model
---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+import keras
+
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = keras.models.load_model("yo.h5", custom_objects=None, compile=True, options=None)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [(int_features)]
+    prediction = model.predict(final_features)
+
+    #output = round(prediction[0][0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(round(prediction[0][0], 2)))
+
+
+if __name__ == "__main__":
+    app.run()
